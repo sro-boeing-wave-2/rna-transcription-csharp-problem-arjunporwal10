63cf95997c8d5dc5ae15782f87e297adada3de1d
arjun <arjunporwal10@gmail.com>
2018-07-19 12:10:26 +0530

    test passed

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace RNATranscriptionModule
 {
@@ -6,7 +7,24 @@ namespace RNATranscriptionModule
     {
         public static string ToRna(string nucleotide)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            //throw new NotImplementedException("You need to implement this function.");
+            //StringBuilder myStringBuilder = new StringBuilder(nucleotide);
+            //myStringBuilder.Replace('G', 'C', 0, myStringBuilder.Length);
+            //myStringBuilder.Replace('C', 'G', 0, myStringBuilder.Length);
+            //myStringBuilder.Replace('T', 'A', 0, myStringBuilder.Length);
+            //myStringBuilder.Replace('A', 'U', 0, myStringBuilder.Length);
+
+            //return myStringBuilder.ToString();
+            string msg = "";
+            foreach (char c in nucleotide)
+            {
+                if (c == 'G') msg += 'C';
+                else if (c == 'C') msg += 'G';
+                else if (c == 'T') msg += 'A';
+                else msg += 'U';
+
+            }
+            return msg;
         }
     }
 }
